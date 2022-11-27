e0b22d8169a1ff7f4d444e77d916172836bd2239
张优强 <lyzyq_997@163.com>
2022-11-27 21:59:35 +0800

    111

--- src/01-helloworld.ts
+++ src/01-helloworld.ts
@@ -1,14 +1,13 @@
-
 (function () {
   // TS文件无法直接运行，只能编译成js才能运行
-  function getter(person:string):string {
-    return 'Hello' + person
+  function getter(person: string): string {
+    return "Hello" + person;
   }
 
-
-  let user = 'user'
-  console.log(getter(user))
+  console.log("测试");
+  let user = "user";
+  console.log(getter(user));
 
   // let user2 = 2
   // console.log(getter(user2)) //error 类型“number”的参数不能赋给类型“string”的参数。
-})()
+})();
